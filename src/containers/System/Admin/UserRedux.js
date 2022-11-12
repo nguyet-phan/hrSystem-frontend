817abf3c84f5dd99c58bd49bc6329424dc881881
NGUYET PHAN <nguyetpt237@gmail.com>
2022-11-13 01:23:18 +0700

    preview-image

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
-import { getAllCodeService } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
 import * as actions from "../../../store/actions";
+import './UserRedux.scss';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 class UserRedux extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            genderArr: []
+            genderArr: [],
+            positionArr: [],
+            roleArr: [],
+            previewImgURL: '',
+            isOpen: false,
         }
     }
 
     async componentDidMount() {
         this.props.getGenderStart();
-
-        // try {
-        //     let res = await getAllCodeService('gender');
-        //     if (res && res.errCode === 0) {
-        //         this.setState({
-        //             genderArr: res.data
-        //         })
-        //     }
-        //     console.log('check all code gender: ', res);
-
-        // } catch (e) {
-        //     console.log(e);
-        // }
+        this.props.getPositionStart();
+        this.props.getRoleStart();
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -37,12 +32,47 @@ class UserRedux extends Component {
                 genderArr: this.props.genderRedux
             })
         }
+        if (prevProps.positionRedux !== this.props.positionRedux) {
+            this.setState({
+                positionArr: this.props.positionRedux
+            })
+        }
+        if (prevProps.roleRedux !== this.props.roleRedux) {
+            this.setState({
+                roleArr: this.props.roleRedux
+            })
+        }
+    }
+
+    handleOnChangeImage = (event) => {
+        let data = event.target.files;
+        let file = data[0];
+
+        if (file) {
+            let objectUrl = URL.createObjectURL(file);
+            this.setState({
+                previewImgURL: objectUrl
+            })
+            console.log('check get file: ', objectUrl);
+        }
+    }
+
+    openPreviewImage = () => {
+        this.setState({
+            isOpen: true
+        })
     }
 
     render() {
-        let gender = this.state.genderArr;
+        let genders = this.state.genderArr;
+        let positions = this.state.positionArr;
+        let roles = this.state.roleArr;
+
+
         let language = this.props.language;
-        console.log('check props redux: ', this.props.genderRedux);
+        let isGetGender = this.props.isLoadingGender;
+
+        console.log('check state component: ', this.state);
         return (
             <div className='user-redux-container'>
                 <div className='title'>
@@ -51,6 +81,9 @@ class UserRedux extends Component {
                 <div className='user-redux-body' >
                     <div className='container'>
                         <div className='row'>
+                            <div className='col-12'>
+                                {isGetGender === true ? 'Loading genders...' : ''}
+                            </div>
                             <div className='col-12 my-3'>
                                 <FormattedMessage id='manage-user.add' />
                             </div>
@@ -81,8 +114,8 @@ class UserRedux extends Component {
                             <div className='col-3'>
                                 <label><FormattedMessage id='manage-user.gender' /></label>
                                 <select className='form-control'>
-                                    {gender && gender.length > 0 &&
-                                        gender.map((item, index) => {
+                                    {genders && genders.length > 0 &&
+                                        genders.map((item, index) => {
                                             return (
                                                 <option key={index}>
                                                     {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
@@ -96,20 +129,46 @@ class UserRedux extends Component {
                             <div className='col-3'>
                                 <label><FormattedMessage id='manage-user.position' /></label>
                                 <select className='form-control'>
-                                    <option selected>Male</option>
-                                    <option>Female</option>
+                                    {positions && positions.length > 0 &&
+                                        positions.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })
+                                    }
                                 </select>
                             </div>
                             <div className='col-3'>
                                 <label><FormattedMessage id='manage-user.role' /></label>
                                 <select className='form-control'>
-                                    <option selected>Male</option>
-                                    <option>Female</option>
+                                    {roles && roles.length > 0 &&
+                                        roles.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })
+                                    }
                                 </select>
                             </div>
                             <div className='col-3'>
                                 <label><FormattedMessage id='manage-user.image' /></label>
-                                <input className='form-control' type='text' />
+                                <div className='preview-img-container'>
+                                    <input id='previewImg' type='file' hidden
+                                        onChange={(event) => { this.handleOnChangeImage(event) }}
+
+                                    />
+                                    <label className='label-upload' htmlFor='previewImg'>
+                                        Tải ảnh lên <i className='fas fa-upload'></i>
+                                    </label>
+                                    <div className='preview-image'
+                                        style={{ backgroundImage: `url(${this.state.previewImgURL})` }}
+                                        onClick={() => this.openPreviewImage()}
+                                    ></div>
+                                </div>
                             </div>
                             <div className='col-12 mt-3'>
                                 <button className='btn btn-primary'>
@@ -118,6 +177,14 @@ class UserRedux extends Component {
                             </div>
                         </div>
                     </div>
+
+                    {this.state.isOpen === true &&
+                        <Lightbox
+                            mainSrc={this.state.previewImgURL}
+                            onCloseRequest={() => this.setState({ isOpen: false })}
+                        />
+                    }
+
                 </div>
             </div>
         )
@@ -129,12 +196,17 @@ const mapStateToProps = state => {
     return {
         language: state.app.language,
         genderRedux: state.admin.genders,
+        positionRedux: state.admin.positions,
+        roleRedux: state.admin.roles,
+        isLoadingGender: state.admin.isLoadingGender
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        getGenderStart: () => dispatch(actions.fetchGenderStart())
+        getGenderStart: () => dispatch(actions.fetchGenderStart()),
+        getPositionStart: () => dispatch(actions.fetchPositionStart()),
+        getRoleStart: () => dispatch(actions.fetchRoleStart()),
         // processLogout: () => dispatch(actions.processLogout()),
         // changeLanguageAppRedux: (language) => dispatch(actions.changeLanguageApp(language)),
     };
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,13 +1,7 @@
 import actionTypes from '../actions/actionTypes';
 
-const initContentOfConfirmModal = {
-    isOpen: false,
-    messageId: "",
-    handleFunc: null,
-    dataFunc: null
-}
-
 const initialState = {
+    isLoadingGender: false,
     genders: [],
     roles: [],
     positions: []
@@ -16,19 +10,46 @@ const initialState = {
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.FETCH_GENDER_START:
-            console.log('fire fetch gender start: ', action);
+            let copySate = { ...state };
+            copySate.isLoadingGender = true;
+            return {
+                ...copySate
+            }
+
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data;
+            state.isLoadingGender = false;
             return {
                 ...state
             }
-        case actionTypes.FETCH_GENDER_SUCCESS:
-            let copyState = { ...state };
-            copyState.genders = action.data;
-            console.log('fire fetch gender success: ', copyState);
-            return {
-                ...copyState
-            }
+
         case actionTypes.FETCH_GENDER_FAILDED:
-            console.log('fire fetch gender failded: ', action);
+            state.genders = [];
+            state.isLoadingGender = false;
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data;
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_POSITION_FAILDED:
+            state.positions = [];
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_ROLE_FAILDED:
+            state.roles = [];
             return {
                 ...state
             }
