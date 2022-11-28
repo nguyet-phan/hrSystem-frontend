4dcb34b8dd7d89742255b1640a1f39ae09ac0968
NGUYET PHAN <nguyetpt237@gmail.com>
2022-11-29 01:44:56 +0700

    user info page

--- src/containers/System/Admin/Department/ManageDepartment.js
+++ src/containers/System/Admin/Department/ManageDepartment.js
@@ -25,61 +25,58 @@ class ManageDepartment extends Component {
     render() {
 
         return (
-            <div className='container' style={{ marginTop: '60px' }}>
+            <div className='user-info ' style={{ marginTop: '60px' }}>
                 <div className='title'>
-                    Quản lý phòng ban
+                    Thông tin cá nhân
                 </div>
-                <div className='form-manage-department form-group'>
-                    <div className='department-name '>
-                        <label>Tên phòng ban</label>
-                        <input className='form-control' type='text'
-                            placeholder='Nhập tiêu đề'
-                        />
+                <div className='container'>
+                    <div className='row d-flex justify-content-center'>
+                        <div className='col-md-10 mt-5 pt-5'>
+                            <div className='row z-depth-3'>
+                                <div className='col-sm-4 bg-info rounded-left'>
+                                    <div className='card-block text-center text-white'>
+                                        <i className='fas fa-user-tie fa-7x mt-5'></i>
+                                        <h2 className='font-weight-bold mt-4'>Admin</h2>
+                                        <p>Super Admin</p>
+                                        <i className='far fa-edit fa-2x mb-4'></i>
+                                    </div>
+                                </div>
+                                <div className='col-sm-8 bg-white rounded-right'>
+                                    <h3 className='mt-3 text-center'>Thông tin cá nhân</h3>
+                                    <hr className='badge-primary mt-0 w-25' />
+                                    <div className='row'>
+                                        <div className='col-sm-6'>
+                                            <p className='font-weight-bold'>Họ đệm: </p>
+                                            <h6 className='text-muted'>Super</h6>
+                                        </div>
+                                        <div className='col-sm-6'>
+                                            <p className='font-weight-bold'>Tên: </p>
+                                            <h6 className='text-muted'>Admin</h6>
+                                        </div>
+                                    </div>
+                                    <h3 className='mt-3'>Thông tin tài khoản</h3>
+                                    <hr className='badge-primary' />
+                                    <div className='row'>
+                                        <div className='col-sm-6'>
+                                            <p className='font-weight-bold'>Email: </p>
+                                            <h6 className='text-muted'>superadmin@gmail.com</h6>
+                                        </div>
+                                        <div className='col-sm-6'>
+                                            <p className='font-weight-bold'>Phone: </p>
+                                            <h6 className='text-muted'>+84123456789</h6>
+                                        </div>
+                                    </div>
+                                    <hr className='badge-primary' />
+                                    <ul className='list-unstyled d-flex justify-content-center mt-4'>
+                                        <li><a href='#'><i className='fab fa-facebook-f px-3 h4 text-info'></i></a></li>
+                                        <li><a href='#'><i className='fab fa-youtube px-3 h4 text-info'></i></a></li>
+                                        <li><a href='#'><i className='fab fa-twitter px-3 h4 text-info'></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='description'>
-                        <label className='title-detail'>Mô tả</label>
-                        <input className='form-control' placeholderText='Chọn ngày'
-                        />
-                    </div>
-
                 </div>
-                <button
-                    className='btn btn-primary px-3'
-                >
-                    <i className='fas fa-plus'></i> Lưu
-                </button>
-                <hr></hr>
-
-                <table className='list-departments'>
-                    <thead>
-                        <tr>
-                            <th>Phòng ban</th>
-                            <th>Quản lý</th>
-                            <th>Mô tả</th>
-                            <th>Số lượng nhân viên</th>
-                            <th>Hành động</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>IT</td>
-                            <td>Admin</td>
-                            <td>Công nghệ thông tin</td>
-                            <td>50</td>
-                            <td>
-                                <button className='btn-edit'
-                                // onClick={() => this.handleEditUser(item)}
-                                ><i className='fas fa-pencil-alt'></i>
-                                </button>
-                                <button className='btn-delete'
-                                // onClick={() => this.handleDeleteUser(item)}
-                                ><i className='fas fa-trash'></i>
-                                </button>
-                            </td>
-                        </tr>
-
-                    </tbody>
-                </table>
             </div>
         );
     }
